99a5679390d47e5b67c2fbb957a84bfe4c72bec2
JuanHerencia <juanluis.herencia@gmail.com>
2026-04-16 17:58:29 -0500

    Clase3-Ejercicio 1 solucion

--- clase03/ejercicio1.java
+++ clase03/ejercicio1.java
@@ -4,12 +4,22 @@
 para obtener la ciudad. Si la estructura de Direccion cambia (ej. 
 se añade un idioma o se separa código postal), Facturador se rompe. 
 */
+// Corrección
+public class Cliente {
+    private Dirección direccion;
+
+    public String getCiudad() {
+        return this.direccion.GetCiudad();
+    }
+}
 
 public class Facturador { 
     public void imprimirFactura(Cliente cliente) { 
         // Depende de la estructura interna de Direccion 
-        String ciudad = cliente.getDireccion().getCiudad();  
-        System.out.println("Ciudad: " + ciudad); 
+        //String ciudad = cliente.getDireccion().getCiudad(); 
+
+        // Con esto solo se accede a lo que se necesita 
+        System.out.println("Ciudad: " + cliente.getCiudad()); 
     } 
 } 
 
